f92766f64d22bd01a66985e76fdb75096be3cf9b
cainarm <cainarm@yahoo.com.br>
2023-01-10 12:52:55 -0300

    infra: removing vercel settings for now :(

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,4 @@
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  future: {},
-  serverBuildTarget: "vercel",
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
 };
